724c80d0e51c57d4999f077422618cf91220b32c
Alif Fatul <aliffatul86tf@gmail.com>
2023-10-24 02:25:42 +0700

    update color dict in `color.py`

--- utility/color.py
+++ utility/color.py
@@ -1,12 +1,22 @@
 def color_label(label: str):
-    if label == "cigarette":
-        return (64, 0, 255), (255, 255, 255)
+    """
+    Generates a color tuple based on the given label.
 
-    if label == "smoke":
-        return (0, 94, 255), (255, 255, 255)  # orange
+    Parameters:
+        label (str): The label for which to generate the color tuple.
 
-    if label == "person":
-        # return (0, 255, 200), (0, 0, 0)  # lime
-        return (255, 65, 0), (255, 255, 255)  # blue
+    Returns:
+        tuple: A color tuple representing the frame color and text color corresponding to the label.
+        If the label is not found in the color dictionary, a default color tuple of red with white text is returned.
+    """
+    color_dict = {
+        # Frame color is purple with white text
+        "cigarette": ((64, 0, 255), (255, 255, 255)),
+        # Frame color is sky blue with white text
+        "smoke": ((0, 94, 255), (255, 255, 255)),
+        # Frame color is orange with white text
+        "person": ((255, 65, 0), (255, 255, 255))
+    }
 
-    return (0, 0, 0), (255, 255, 255)
+    # Frame color is red with white text if label not found
+    return color_dict.get(label, ((255, 0, 0), (255, 255, 255)))
