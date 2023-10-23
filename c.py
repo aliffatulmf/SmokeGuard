16abec804a225a6776f3607b26fdaab788f97167
Alif Fatul <aliffatul86tf@gmail.com>
2023-10-23 13:57:50 +0700

    add type annotations

--- c.py
+++ c.py
@@ -3,57 +3,8 @@ import os
 import shutil
 import subprocess
 import sys
-
 import requests
-
-
-def clean_cache():
-    for dirPath, _, _ in os.walk("."):
-        if dirPath.endswith("__pycache__"):
-            print(f"Removing {dirPath}")
-            shutil.rmtree(dirPath)
-
-
-def check_online():
-    import socket
-
-    try:
-        socket.create_connection(("1.1.1.1", 443), 5)
-        return True
-    except OSError:
-        pass
-
-    return False
-
-
-def sanitize_text(text):
-    return text.split("#")[0].strip()
-
-
-def get_requirements():
-    if not check_online():
-        print("You are offline. Please connect to the internet first.")
-        sys.exit(1)
-
-    url = "https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt"
-
-    response = requests.get(url)
-
-    requirements = []
-    for line in response.text.split("\n"):
-        line = line.strip()
-        if line and not line.startswith("#"):
-            requirements.append(line)
-
-    return requirements
-
-
-def install_dependency():
-    deps = get_requirements()
-    for dep in deps:
-        print("Installing", dep)
-        subprocess.run([sys.executable, "-m", "pip", "install", dep])
-
+import socket
 
 FMT_OPT = {
     "formatter": "yapf",
@@ -61,36 +12,63 @@ FMT_OPT = {
     "inplace": True,
 }
 
+def remove_cache() -> None:
+    for dirPath, _, _ in os.walk("."):
+        try:
+            if dirPath.endswith("__pycache__"):
+                print(f"Removing {dirPath}")
+                shutil.rmtree(dirPath)
+        except Exception as e:
+            print(f"Failed to remove {dirPath} due to {str(e)}")
 
-def formatter(path: str):
+def is_online() -> bool:
+    try:
+        socket.create_connection(("1.1.1.1", 443), 2)
+        return True
+    except OSError as err:
+        print(f"OS Error: {err}")
+    return False
+
+def clean_text(text: str) -> str:
+    return text.split("#")[0].strip()
+
+def fetch_requirements() -> list:
+    if not is_online():
+        print("You are offline. Please connect to the internet first.")
+        sys.exit(1)
+
+    url: str = "https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt"
+    response = requests.get(url)
+    requirements: list = [line.strip() for line in response.text.split("\n") if line and not line.startswith("#")]
+    return requirements
+
+def install_dependencies() -> None:
+    deps: list = fetch_requirements()
+    for dep in deps:
+        print("Installing", dep)
+        subprocess.run([sys.executable, "-m", "pip", "install", dep])
+
+def format_code(path: str) -> None:
     print("Formatting", path)
     subprocess.run(["yapf", "-i", path])
 
-
 def main():
     parser = argparse.ArgumentParser(description="Control Panel for YOLOv5")
-    parser.add_argument("-cc", "--clean-cache", action="store_true", help="Clean the cache folder")
-    parser.add_argument("--install-dependency", action="store_true", help="Install dependencies")
-
+    parser.add_argument("-c", "--clear-cache", action="store_true", help="Clean the Python cache folder")
+    parser.add_argument("-i", "--install-deps", action="store_true", help="Install YOLOv5 dependencies")
     fmt_group = parser.add_argument_group(title="format arguments")
-    fmt_group.add_argument("-fi", "--fmt-input", type=str, help="Input file or directory")
-
+    fmt_group.add_argument("-f", "--fmt-input", type=str, help="Input file or directory to format")
     args = parser.parse_args()
 
     handlers = {
-        "clean_cache": clean_cache,
-        "install_dependency": install_dependency,
-        "fmt_input": lambda: formatter(args.fmt_input),
+        "clear_cache": remove_cache,
+        "install_deps": install_dependencies,
+        "fmt_input": lambda: format_code(args.fmt_input),
     }
 
     for key, value in vars(args).items():
         if value:
-            if isinstance(handlers[key], list):
-                for handler in handlers[key]:
-                    handler()
-            else:
-                handlers[key]()
-
+            handlers[key]()
 
 if __name__ == "__main__":
     main()
