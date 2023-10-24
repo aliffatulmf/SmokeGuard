ab9e0609af5afafacce9b7f81e9201a10e82aaac
Alif Fatul <aliffatul86tf@gmail.com>
2023-10-24 22:18:13 +0700

    update formatting

--- control_panel.py
+++ control_panel.py
@@ -1,16 +1,18 @@
 import argparse
 import os
 import shutil
+import socket
 import subprocess
 import sys
+
 import requests
-import socket
 
 FMT_OPT = {
     "formatter": "yapf",
     "style": "google",
     "inplace": True,
 }
+
 
 def remove_cache() -> None:
     for dirPath, _, _ in os.walk("."):
@@ -21,6 +23,7 @@ def remove_cache() -> None:
         except Exception as e:
             print(f"Failed to remove {dirPath} due to {str(e)}")
 
+
 def is_online() -> bool:
     try:
         socket.create_connection(("1.1.1.1", 443), 2)
@@ -29,15 +32,19 @@ def is_online() -> bool:
         print(f"OS Error: {err}")
     return False
 
+
 def clean_text(text: str) -> str:
     return text.split("#")[0].strip()
+
 
 def fetch_requirements() -> list:
     if not is_online():
         print("You are offline. Please connect to the internet first.")
         sys.exit(1)
 
-    url: str = "https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt"
+    url: str = (
+        "https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt"
+    )
     try:
         response = requests.get(url, timeout=5)
         response.raise_for_status()
@@ -46,12 +53,17 @@ def fetch_requirements() -> list:
         sys.exit(1)
 
     try:
-        requirements: list = [line.strip() for line in response.text.split("\n") if line and not line.startswith("#")]
+        requirements: list = [
+            line.strip()
+            for line in response.text.split("\n")
+            if line and not line.startswith("#")
+        ]
     except Exception as err:
         print(f"Error occurred while parsing the response: {err}")
         sys.exit(1)
 
     return requirements
+
 
 def install_dependencies() -> None:
     deps: list = fetch_requirements()
@@ -59,16 +71,24 @@ def install_dependencies() -> None:
         print("Installing", dep)
         subprocess.run([sys.executable, "-m", "pip", "install", dep])
 
+
 def format_code(path: str) -> None:
     print("Formatting", path)
     subprocess.run(["yapf", "-i", path])
 
+
 def main():
     parser = argparse.ArgumentParser(description="Control Panel for YOLOv5")
-    parser.add_argument("-c", "--clear-cache", action="store_true", help="Clean the Python cache folder")
-    parser.add_argument("-i", "--install-deps", action="store_true", help="Install YOLOv5 dependencies")
+    parser.add_argument(
+        "-c", "--clear-cache", action="store_true", help="Clean the Python cache folder"
+    )
+    parser.add_argument(
+        "-i", "--install-deps", action="store_true", help="Install YOLOv5 dependencies"
+    )
     fmt_group = parser.add_argument_group(title="format arguments")
-    fmt_group.add_argument("-f", "--fmt-input", type=str, help="Input file or directory to format")
+    fmt_group.add_argument(
+        "-f", "--fmt-input", type=str, help="Input file or directory to format"
+    )
     args = parser.parse_args()
 
     handlers = {
@@ -80,6 +100,7 @@ def main():
     for key, value in vars(args).items():
         if value:
             handlers[key]()
+
 
 if __name__ == "__main__":
     main()
