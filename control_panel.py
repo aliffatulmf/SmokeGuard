46ac2f7cbcbdb93797168b3e3d657e1a011f58f9
Alif Fatul <aliffatul86tf@gmail.com>
2024-02-13 16:46:12 +0700

    refactor control_panel

--- control_panel.py
+++ control_panel.py
@@ -5,56 +5,69 @@ import textwrap
 
 from rich.logging import RichHandler
 
-MENU = textwrap.dedent("""Smoker Control Panel
+MENU = textwrap.dedent("""
+Smoker Control Panel
 
-options:
-    run             - start the program
-    clean           - clean the cache to free up storage
-    help, --help    - show this help message and exit
-    """)
-
+Options:
+   run             - Start the program
+   clean           - Clean the cache to free up storage
+   help, --help    - Show this help message and exit
+""")
 
 def setup_hub():
-    import os
-    import shutil
+   if not os.path.exists("hub"):
+       import shutil
 
-    if not os.path.exists("hub"):
-        from git import Repo
+       import git
 
-        print("="*10, "CLONE YOLOv5", "="*10)
-        
-        os.makedirs("hub", 777)
-        repo_url = "https://github.com/ultralytics/yolov5"
-        repo = Repo.clone_from(repo_url, "hub")
+       print("=" * 10, "CLONE YOLOv5", "=" * 10)
 
-    if not os.path.exists("utils"):
-        shutil.move("hub/utils", "utils")
+       os.makedirs("hub", 0o777)
+       repo_url = "https://github.com/ultralytics/yolov5"
+       repo = git.Repo.clone_from(repo_url, "hub")
 
-    if not os.path.exists("models"):
-        shutil.move("hub/models", "models")
+   if not os.path.exists("utils"):
+       shutil.move("hub/utils", "utils")
 
-    if not os.path.exists("export.py"):
-        shutil.move("hub/export.py", ".")
+   if not os.path.exists("models"):
+       shutil.move("hub/models", "models")
 
+   if not os.path.exists("export.py"):
+       shutil.move("hub/export.py", ".")
+
+def print_menu():
+   print(MENU)
+
+def clean_cache():
+   from meta.cache import clean_cache_dir
+   clean_cache_dir(".")
+
+def start_program():
+   from meta.exec import window
+   window()
+
+def main():
+   setup_hub()
+
+   logging.basicConfig(
+       level=logging.NOTSET,
+       format="%(message)s",
+       datefmt="[%X]",
+       handlers=[RichHandler(omit_repeated_times=False)],
+   )
+
+   arg = sys.argv[1] if len(sys.argv) > 1 else "help"
+
+   functions = {
+       "help": print_menu,
+       "clean": clean_cache,
+       "run": start_program,
+   }
+
+   if arg.lower() in functions:
+       functions[arg.lower()]()
+   else:
+       print_menu()
 
 if __name__ == "__main__":
-    setup_hub()
-
-    logging.basicConfig(
-        level=logging.NOTSET,
-        format="%(message)s",
-        datefmt="[%X]", handlers=[RichHandler(omit_repeated_times=False)],
-    )
-
-    arg = sys.argv[1] if len(sys.argv) > 1 else "help"
-
-    if arg.lower() in ["help", "--help"]:
-        print(MENU)
-    elif arg.lower() == "clean":
-        from meta.cache import clean_cache_dir
-        clean_cache_dir(".")
-    elif arg.lower() == "run":
-        from meta.exec import window
-        window()
-    else:
-        print(MENU)
+   main()
